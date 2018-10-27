37383bc9c9c023a97ad46f6552c6e9ef6b4cc82a
Jesper Orb <jesper.orb@zocom.se>
2018-10-27 13:33:05 +0200

    Hook up Controller to startup script

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using RoomBooking.Models;
 
 namespace RoomBooking
 {
@@ -20,6 +22,8 @@ namespace RoomBooking
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<RoomContext>(opt => 
+                opt.UseInMemoryDatabase("Rooms"));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             // In production, the React files will be served from this directory
